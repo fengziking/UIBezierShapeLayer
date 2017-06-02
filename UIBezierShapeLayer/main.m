6c13718c7981bff7a90f7ccb9dbdf1ccc64f84e5
fengziking <794505782@qq.com>
2017-06-02 15:18:32 +0800

    Initial Commit

---
+++ UIBezierShapeLayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIBezierShapeLayer
+//
+//  Created by 刘建明 on 17/6/2.
+//  Copyright © 2017年 刘建明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
